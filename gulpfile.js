0fa8ca87955eeff7e050e9e1543d3642abebf2b1
Nikita Tsyhanenko <runninzero@gmail.com>
2016-06-14 12:24:47 +0300

    configure creating of source maps

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('minify', function() {
     return gulp.src('./src/*.js')
+        .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist'));
 });
 
